2e5a8ba368a3f7c946530ee67009fb058dbc8138
Jorge Rodríguez <jrodriguezubb@gmail.com>
2022-02-05 11:46:43 -0300

    valid jwt

--- routes/customer.js
+++ routes/customer.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const { getCustomers, createCustomer } = require('../controllers/customer')
+const { validatorJWT } = require('../middlewares/validator-jwt')
 
 const router = Router();
 
-router.get( '/', getCustomers
+router.get( '/', validatorJWT, getCustomers
 );
 
 router.post( '/', 
